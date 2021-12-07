ece8859269d044edeade0e4cdbde74a18787f450
Kjell Post <kjell@irstafoto.se>
2021-12-07 10:41:56 +0000

    Solution for day 7

--- 2021/7/7a.py
+++ 2021/7/7a.py
@@ -3,7 +3,6 @@
 data = list(map(int, open("data.txt").read().split(",")))
 
 def cost(p):
-    global data
     return sum([abs(d-p) for d in data])
 
 print(min([cost(p) for p in range(min(data),max(data))]))
--- 2021/7/7b.py
+++ 2021/7/7b.py
@@ -6,7 +6,6 @@ def fuel(n):     # 1+2+...+n = n(n+1)/2
     return int(n*(n+1)/2)
 
 def cost(p):
-    global data
     return sum([fuel(abs(d-p)) for d in data])
 
 print(min([cost(p) for p in range(min(data),max(data))]))
