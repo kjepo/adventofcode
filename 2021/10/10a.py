c26bb1fb87a6241a38b31738bccc4b138cee0ff5
Kjell Post <kjell@irstafoto.se>
2021-12-10 15:39:38 +0000

    Solutions to day 10

---
+++ 2021/10/10a.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# Grammar
+# E --> T E | epsilon
+# T --> [ E ]   |  ( E )  |  < E >  | ( E ) |  epsilon
+
+import sys
+
+score = 0
+errorscore = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+closing = { "{": "}", "(": ")", "[": "]", "<": ">" }
+
+# simple scanner: sets c to next character or "#" if EOL if we've reached end of line
+def scan(): 
+    global line, c
+    if len(line) == 0:
+        c = "#"
+    else:
+        c = line[0]
+        if c in "{([<>])}":
+            line = line[1:]
+        else:                   # if not valid character, line is corrupt... so EOL
+            c = "#"
+
+def E():
+    if c == "#":
+        return
+    if c in "[{<(":
+        T()
+        E()
+
+def T():
+    global c, line, score
+    opening = c
+    scan()
+    E()
+    if c == closing[opening]:
+        scan()
+    else:
+        # print("Syntax error: ", c, "expected", closing[opening])
+        if c in errorscore:
+            score += errorscore[c]
+        line = ["EOF"]
+        c = "EOF"
+
+totalscores = []
+lines = open(sys.argv[1]).readlines()
+for line in lines:
+    line = line.strip()
+    # print(line)
+    scan()
+    E()
+    if score > 0:
+        totalscores.append(score)
+    score = 0
+# print(totalscores)
+print(sum(totalscores))
---
+++ 2021/10/10b.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Grammar
+# E --> T E | epsilon
+# T --> [ E ]   |  ( E )  |  < E >  | ( E ) |  epsilon
+
+import sys
+
+score = 0
+completion_score = { ')': 1, ']': 2, '}': 3, '>': 4 }
+closing = { "{": "}", "(": ")", "[": "]", "<": ">" }
+
+# simple scanner: sets c to next character or "#" if EOL if we've reached end of line
+def scan(): 
+    global line, c
+    if len(line) == 0:
+        c = "#"
+    else:
+        c = line[0]
+        if c in "{([<>])}":
+            line = line[1:]
+        else:
+            c = "#"
+
+def E():
+    if c == "#":
+        return
+    if c in "[{<(":
+        T()
+        E()
+
+def T():
+    global c, line, score
+    opening = c
+    scan()
+    E()
+    if c == closing[opening]:
+        scan()
+    else:
+        # print("Syntax error: ", c, "expected", closing[opening])
+        if c == "#" and score >= 0: # auto-complete
+            score = score*5 + completion_score[closing[opening]]
+        else:                # syntax error: don't calculate auto-complete score
+            score = -1
+            line = []
+
+totalscores = []
+lines = open(sys.argv[1]).readlines()
+for line in lines:
+    line = line.strip()
+    # print(line)
+    scan()
+    E()
+    if score > 0:
+        totalscores.append(score)
+    score = 0
+totalscores.sort()
+# print(totalscores)
+print(totalscores[int(len(totalscores)/2)])
