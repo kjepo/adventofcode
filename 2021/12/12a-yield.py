c53ca5f408c52c8f3c5ca0482d0d738c9af44dee
Kjell Post <kjell@irstafoto.se>
2021-12-13 00:47:37 +0000

    Using Python generators and DFS

---
+++ 2021/12/12a-yield.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+
+# 12A solved with another data structure and using Python generators
+
+G = { }   # G[u] = all nodes v such that u-v is an edge in the graph
+
+def big(cave):
+    return cave[0] >= 'A' and cave[0] <= 'Z'
+
+def dfs(u, path):
+    if u == 'end':
+        yield path + [u]
+    else:
+        for v in G[u]:
+            if big(v) or v not in path + [u]:
+                yield from dfs(v, path + [u])
+
+if __name__ == "__main__":
+    nodes = []
+    lines = open(sys.argv[1]).readlines()
+    # read undirected graph 
+    for line in lines:          
+        [u,v] = line.strip().split("-")
+        if u not in G:
+            G[u] = []
+        if v not in G:
+            G[v] = []
+        G[u].append(v)
+        G[v].append(u)
+    # print(G)
+    for p in dfs('start', []):
+        print(p)
