a76585471d3109c8db73ec0587820d020e72eb7a
Kjell Post <kjell@irstafoto.se>
2021-12-17 20:14:45 +0000

    Boilerplate code for AoC

--- 2021/boilerplate/regex.py
+++ 2021/boilerplate/regex.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+# parses input like this into array of numbers
+#
+# 0,9 -> 5,9
+# 8,0 -> 0,8
+# 9,4 -> 3,4
+# etc
+#
+# change [a,b,c,d] to, e.g., [a,b] if you know there are
+# only two numbers per line. Numbers can be negative and
+# surrounded by all kinds of crap, i.e., this also works
+#
+# 0 9 -- 5 9
+# 8 0 -- 0 8
+# 9 4 -- 3 4
+# etc
+
 import sys
 import re
 
 def solve(a,b,c,d):
-    print("%d,%d -> %d,%d" % (a,b,c,d))
+    print(a,b,c,d)
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         for line in f.read().splitlines():
-            [a,b,c,d] = map(int, re.findall(r'[-]?\d+', line))
-            solve(a,b,c,d)
+            if line:
+                [a,b,c,d] = map(int, re.findall(r'[-]?\d+', line))
+                solve(a,b,c,d)
